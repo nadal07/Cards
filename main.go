a0fe8e59ee4dacc569f1321cf7dbf3e8574cf806
Nadeem Khan <mnadeemak@gmail.com>
2023-04-05 21:30:12 +0530

    Variable Declarations

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"   // := is used for intilization
+	card = "Five of Diamonds" // = assigning
+	fmt.Println(card)
 }
